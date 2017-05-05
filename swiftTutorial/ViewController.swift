73045d854a4db9b95e17a728eafb5f99774d821a
Bogdan Adrian Galea <bogdan.galea84@gmail.com>
2017-05-05 12:10:40 +0200

    committed

---
+++ swiftTutorial/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  swiftTutorial
+//
+//  Created by Bogdan A Galea on 2017-05-05.
+//  Copyright © 2017 WhiteBelt Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        //theLabel.text = "Calculate"
+        tapCount = tapCount + 1
+        print(tapCount)
+        if tapCount >= 10 {
+            theLabel.text = "You Tapped 10 times"
+        }
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
